8c39a76dc7a13de965adcb8f2068a59a47ab5ab0
Andrew Aday <andrewladay@gmail.com>
2015-05-25 04:51:00 -0400

    minor changes

--- spotify_get_playlists.php
+++ spotify_get_playlists.php
@@ -10,13 +10,13 @@ require 'includes/error_handling.php';
 	$api->setAccessToken("BQD0Nhr6Ol2TfGKXFw2L3KkbwcYY2Y35S-AvFRVhTBF4Qaq8MyIKkyVPeqskam6igUKwTSoHJLH_TYLOj4CHzoAv6O0R68SkmSnWQGp8VhATGZcQ4aWcHVyFOFpBGEhG2cT6zm4cexOuDnzMAWO5UzE0OTRiwFw8nMfntvqc6-wzz3JDuAuC0FHGuw4LHTiiz1Bh3nl0li1rfpZafaZwpDzrj5u75-fLyOfnQaVsUDOx_69XO-19SQgiY-BT23_bCt3blN4RTEiIPDiY4w-D4Kpy");
 
 	$me = $api->me();
-	print_r($me);
+	print_r(json_encode($me));
 	
 	echo '</br>';
 	
 	$playlists = $api->getUserPlaylists('username');
 
-	print_r($playlists);
+	print_r(json_encode($playlists));
 
 // } else {
 // 	report_error('error', 'improper params', 'spotify_get_playlists');
