3f165ce69f46ab6ce13bd1c54eb96d2d5e588596
Andrew Aday <andrewladay@gmail.com>
2015-05-25 02:18:06 -0700

    more estuff

--- spotify_get_playlists.php
+++ spotify_get_playlists.php
@@ -10,9 +10,11 @@ if (isset($_POST['token'])) {
 	// $api->setAccessToken("BQD0Nhr6Ol2TfGKXFw2L3KkbwcYY2Y35S-AvFRVhTBF4Qaq8MyIKkyVPeqskam6igUKwTSoHJLH_TYLOj4CHzoAv6O0R68SkmSnWQGp8VhATGZcQ4aWcHVyFOFpBGEhG2cT6zm4cexOuDnzMAWO5UzE0OTRiwFw8nMfntvqc6-wzz3JDuAuC0FHGuw4LHTiiz1Bh3nl0li1rfpZafaZwpDzrj5u75-fLyOfnQaVsUDOx_69XO-19SQgiY-BT23_bCt3blN4RTEiIPDiY4w-D4Kpy");
 
 	$me = $api->me();
-	echo json_encode($me);
-	
-	// $playlists = $api->getUserPlaylists('1234086604');
+	$username = $me['id'];
+
+	$playlists = $api->getUserPlaylists($username, array('limit' => 50));
+
+	echo json_encode($playlists);
 
 
 } else {
