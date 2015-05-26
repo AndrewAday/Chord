14992d50426f5616ff7b98478a49f174f45a57a5
Andrew Aday <andrewladay@gmail.com>
2015-05-26 01:56:30 -0700

    added name and artist to tracks

--- chord_update_playlist.php
+++ chord_update_playlist.php
@@ -3,18 +3,22 @@
 require_once 'includes/db_connect.php';
 require_once 'includes/error_handling.php';
 
-if (isset($_POST['username']) and isset($_POST['playlist_id']) and isset($_POST['track_id']) and isset($_POST['type'])) {
+if (isset($_POST['username']) and isset($_POST['playlist_id']) and isset($_POST['track_id']) and isset($_POST['type']) and isset($_POST['name']) and isset($_POST['artist'])) {
 
 	$username = $_POST['username'];
 	$playlist = $_POST['playlist_id'];
 	$track = $_POST['track_id'];
 	$type = $_POST['type'];
+	$name = $_POST['name'];
+	$artist = $_POST['artist'];
 
 	$user = $users->findOne(['username' => $username]);
 
 	$track_update = [
 		'type' => $type,
 		'id' => $track
+		'name' => $name,
+		'artist' => $artist
 	];
 
 	try {
