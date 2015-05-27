0bc4c8542556d9fb5a0f2720a7fff6cb23ca89f3
Andrew Aday <andrewladay@gmail.com>
2015-05-26 19:26:55 -0700

    minor bug

--- soundcloud_get_playlists.php
+++ soundcloud_get_playlists.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL | E_STRICT);
     ini_set("display_errors", true);
 
-require 'vendor/auto_load.php';
+require 'vendor/autoload.php';
 require_once 'includes/db_connect.php';
 require_once 'includes/error_handling.php';
 
