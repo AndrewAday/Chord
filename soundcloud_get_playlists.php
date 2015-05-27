0eb7316ff35f189d23a40229e0f864488273549f
Andrew Aday <andrewladay@gmail.com>
2015-05-26 19:36:20 -0700

    stuff

--- soundcloud_get_playlists.php
+++ soundcloud_get_playlists.php
@@ -8,7 +8,7 @@ require_once 'includes/db_connect.php';
 require_once 'includes/error_handling.php';
 
 // if (isset($_POST['username'])) {
-	$username = $_POST['username'];
+	// $username = $_POST['username'];
 	$username = "";
 	$user = $users->findOne(['username' => $username]);
 	$access_token = $user['soundcloud_access_token'];
