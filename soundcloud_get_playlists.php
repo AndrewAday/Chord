e1dc988fd06a627109a99210aaa51e4885cb2b1e
Andrew Aday <andrewladay@gmail.com>
2015-05-26 19:24:50 -0700

    get playlists test

---
+++ soundcloud_get_playlists.php
@@ -0,0 +1,40 @@
+<?php 
+
+require 'vendor/auto_load.php';
+require_once 'includes/db_connect.php';
+require_once 'includes/error_handling.php';
+
+// if (isset($_POST['username'])) {
+	$username = $_POST['username'];
+
+	$user = $users->findOne(['username' => $username]);
+	$access_token = $user['soundcloud_access_token'];
+	$access_token = "1-131075-155055189-cbb4df746fe9d";
+
+	$client = new Services_Soundcloud('c30dae074d60ef264d9940a2d7354b33', 'ca7ee5c12c56dd196cd618958bc32302');
+	$client->setAccessToken($access_token);
+
+	$soundcloud_user = json_decode($client->get('me'));
+	$souncloud_id = $soundcloud_user->id;
+
+	// $playlists = json_decode($client->get('users/' . $souncloud_id . '/playlists'));
+	// $tracks = json_decode($client->get('users/' . $souncloud_id . '/tracks'));
+	// $favorites = json_decode($client->get('users/' . $souncloud_id . '/favorites'));
+	print_r($client->get('users/' . $souncloud_id . '/playlists'));
+	echo '</br>';
+	echo '</br>';
+	print_r($client->get('users/' . $souncloud_id . '/tracks'));
+	echo '</br>';
+	echo '</br>';
+	print_r($client->get('users/' . $souncloud_id . '/favorites'));
+
+
+
+
+// } else {
+// 	report_error('error', 'improper params', 'soundcloud_get_playlists');
+// 	exit;
+// }
+
+
+?>
